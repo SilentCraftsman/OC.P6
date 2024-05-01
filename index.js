eb59df39fd4c8a53727275953de3bd665ed7b162
SilentCraftsman <ggambier72@gmail.com>
2024-05-01 19:25:00 +0200

    Test rapide du server

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extends: true }));
-app.use(express.static("uploads"));
-
 const PORT = process.env.PORT || 4000;
+
+app.get("/", function (req, res) {
+  res.send("hello");
+});
 
 app.listen(PORT, function () {
   console.log("Le port est : " + PORT);
