3936fe2a4f885a9908080ee89a417ca2a63f4f8f
SilentCraftsman <ggambier72@gmail.com>
2024-05-01 17:36:37 +0200

    Ajout de quelques middlewares dans index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+app.use(express.static("uploads"));
 
 const PORT = process.env.PORT || 4000;
 
