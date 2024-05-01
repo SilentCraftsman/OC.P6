1c7c7b77b255c1a6de639d2c8480ce999c21ca26
SilentCraftsman <ggambier72@gmail.com>
2024-05-01 19:48:06 +0200

    Test de l'inscription avec CORS et le body de req en JSON

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("hello");
 });
 
+app.post("/api/auth/signup", signUp);
+
 app.listen(PORT, function () {
   console.log("Le port est : " + PORT);
 });
+
+function signUp(req, res) {
+  const body = req.body;
+  console.log(body);
+}
